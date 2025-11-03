a3ac9a741a366eb264449f42aa3802ca16e559ec
shubh6-max <puranikshubham87@gmail.com>
2025-11-03 21:19:22 +0530

    update

--- scripts/update_designations.py
+++ scripts/update_designations.py
@@ -24,11 +24,11 @@ from dotenv import load_dotenv
 load_dotenv()  # loads local .env if running manually
 
 # Fetch values
-PG_USER = os.getenv("PGUSER")
-PG_PASSWORD = os.getenv("PGPASSWORD")
-PG_HOST = os.getenv("PGHOST")
+PG_USER = os.getenv("PG_USER")
+PG_PASSWORD = os.getenv("PG_PASSWORD")
+PG_HOST = os.getenv("PG_HOST")
 PG_PORT = os.getenv("PGPORT", "5432")
-PG_DB = os.getenv("PGDATABASE")
+PG_DB = os.getenv("PG_DB")
 
 AZURE_ENDPOINT = os.getenv("AZURE_ENDPOINT")
 AZURE_DEPLOYMENT = os.getenv("AZURE_DEPLOYMENT")
